acbd2587923cf5d0d377b1823dd588b9061fc0fc
Vatsalkayastha <vatsal.k2@ahduni.edu.in>
2024-04-02 08:24:14 +0530

    Components

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,12 @@
-import { Provider } from 'react-redux'
-import './App.css'
-import store from "./redux/store"
-
+import "./App.css";
+import Todo from "./components/Todo";
 
 function App() {
-
   return (
-    <Provider store={store}>
-      <div className='font-bold text-[32px]'>Hello Developer..</div>
-    </Provider>
-  )
+    <>
+      <Todo />
+    </>
+  );
 }
 
-export default App
+export default App;
